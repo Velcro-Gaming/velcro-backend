98a45f0de69200b0d6152590e64e5f9e3cdbc42a
OBAA <itsobaa#gmail.com>
2021-05-15 08:04:47 +0100

    Moved velcro backend into a docker container for easy management and deployment

---
+++ engine/settings/docker/local.py
@@ -0,0 +1,13 @@
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+# CORS_ORIGIN_WHITELIST = (
+#     'http://velcrogaming.local:3000',
+#     'http://dashboard.velcrogaming.local:3000',
+#     'https://velcrogaming.local:3000',
+#     'https://dashboard.velcrogaming.local:3000',
+# )
+
---
+++ engine/settings/docker/production.py
@@ -0,0 +1,11 @@
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+CORS_ORIGIN_WHITELIST = (
+    'http://velcrogaming.com',
+    'http://dashboard.velcrogaming.com',
+    'https://velcrogaming.com',
+    'https://dashboard.velcrogaming.com',
+)
+
