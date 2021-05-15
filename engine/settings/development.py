3a502ea8c2412b059696ba7c37f44f1e578a3de3
OBAA <itsobaa#gmail.com>
2021-05-15 08:12:46 +0100

    Moved velcro backend into a docker container for easy management and deployment

--- engine/settings/development.py
+++ engine/settings/development.py
@@ -1,11 +1,4 @@
 
-# Get Environment File
-get_secret = os.getenv
-
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_secret('SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
